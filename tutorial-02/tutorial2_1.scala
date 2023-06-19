b6037d5fbd261c7acc61cb08e81c4130ff22f8c3
Nethmi Athukorala <nethmihao2001@gmail.com>
2023-06-19 17:11:22 +0530

    create a file

---
+++ tutorial-02/tutorial2_1.scala
@@ -0,0 +1,27 @@
+object calculator{
+def main(args:Array[String])={
+
+
+var k,i,j:Int =2
+var m,n:Int=5
+var f:Float=12.0f
+var g :Float= 4.0f
+var c: Char ='X'
+
+var expressionA:Int =(k + 12 )* m
+var expressionB:Int=m / j
+var expressionC:Int=n % j
+var expressionD:Int=m / j * j
+var expressionE:Float= f + 10*5 +g
+i+=1
+var expressionF: Int=i* n
+
+println(expressionA)
+println(expressionB)
+println(expressionC)
+println(expressionD)
+println(expressionE)
+println(expressionF)
+
+}
+}
